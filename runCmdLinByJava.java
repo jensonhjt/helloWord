500cd1f51a3f44ebb9bcf9eaf11caa67cdfa30c8
jensonhjt <35219292+jensonhjt@users.noreply.github.com>
2019-03-12 19:38:09 +0800

    Create runCmdLinByJava.java

---
+++ runCmdLinByJava.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class runCmdLineByJava {
+    public static void main(String args[]) {
+        Process process = null;
+        List<String> processList = new ArrayList<String>();
+        try {
+            process = Runtime.getRuntime().exec("id");
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while ((line = input.readLine()) != null) {
+                processList.add(line);
+            }
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : processList) {
+            System.out.println(line);
+        }
+    }
+}
